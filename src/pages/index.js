077be8a98113c1c5863af98fa27b1eea7a784063
김정환 <iz4blue@users.noreply.github.com>
2020-01-12 14:36:05 +0900

    상수 부분과 변수 부분을 구분함

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 
 export default () => {
-  const date_now = new Date();
   const date_first_2020 = new Date(2020, 0, 1, 0, 0, 0, 0);
   const date_last_2020 = new Date(2020, 11, 31, 23, 59, 59, 999);
-
   const ts_2020_total = date_last_2020.getTime() - date_first_2020.getTime();
-  const ts_now_from_first_2020 = date_now.getTime() - date_first_2020.getTime();
 
-  const percent = ts_now_from_first_2020 / ts_2020_total * 100
+  let date_now = new Date();
+  let ts_now_from_first_2020 = date_now.getTime() - date_first_2020.getTime();
+  let percent = ts_now_from_first_2020 / ts_2020_total * 100
   console.log(percent)
 
   return (
