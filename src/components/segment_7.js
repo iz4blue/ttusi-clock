d8f5462db6d0a58d2c26f7fde4804d7e8c2486f5
김정환 <iz4blue@users.noreply.github.com>
2020-01-21 21:53:03 +0900

    '.' 을 대신해서 o 라는 css 를 추가함

--- src/components/segment_7.js
+++ src/components/segment_7.js
@@ -6,16 +6,17 @@ import styles from "../styles/segment_7.module.css"
 
 function getStyle(value) {
   switch(value) {
-    case 0: return styles.displayNo0
-    case 1: return styles.displayNo1
-    case 2: return styles.displayNo2
-    case 3: return styles.displayNo3
-    case 4: return styles.displayNo4
-    case 5: return styles.displayNo5
-    case 6: return styles.displayNo6
-    case 7: return styles.displayNo7
-    case 8: return styles.displayNo8
-    case 9: return styles.displayNo9
+    case 0: case '0': return styles.displayNo0
+    case 1: case '1': return styles.displayNo1
+    case 2: case '2': return styles.displayNo2
+    case 3: case '3': return styles.displayNo3
+    case 4: case '4': return styles.displayNo4
+    case 5: case '5': return styles.displayNo5
+    case 6: case '6': return styles.displayNo6
+    case 7: case '7': return styles.displayNo7
+    case 8: case '8': return styles.displayNo8
+    case 9: case '9': return styles.displayNo9
+    case '.': return styles.displayO
     default:
       return styles.displayNo0
   }
